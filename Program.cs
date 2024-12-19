40a4a8d50d0fb6a14e47f550b14610044f40b7e0
Ilija Q <79657913+IlijaQ@users.noreply.github.com>
2024-12-19 20:07:55 +0100

    add SignalR server setup in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace CandidateLog
 {
     internal static class Program
     {
+        private static IDisposable _signalRServer;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +19,15 @@ namespace CandidateLog
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            StartSignalRServer();
             Application.Run(new Candidates());
+        }
+
+        public static void StartSignalRServer()
+        {
+            string url = "http://localhost:8080";
+            _signalRServer = WebApp.Start<Startup>(url);
+            MessageBox.Show("Server started at " + url);
         }
     }
 }
---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateLog
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HubConfiguration
+            {
+                EnableDetailedErrors = true
+            };
+
+            app.MapSignalR("/signalr", config);
+        }
+    }
+}
